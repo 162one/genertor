99cd895c8b364a069132c34dbc3cee12f3fe6a02
wz <961751713@qq.com>
2022-12-30 16:14:03 +0800

    first commit

---
+++ src/main/java/com/generator/config/DbConfig.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2018 同云开源 http://www.tongyun.io
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.generator.config;
+
+import com.generator.dao.*;
+import com.generator.utils.RRException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.annotation.Resource;
+
+/**
+ * 数据库配置
+ *
+ * @author Mark sunlightcs@gmail.com
+ * @since 2018-07-24
+ */
+@Configuration
+public class DbConfig {
+    @Value("${tongyun.database: mysql}")
+    private String database;
+    @Resource
+    private MySQLGeneratorDao mySQLGeneratorDao;
+    @Resource
+    private OracleGeneratorDao oracleGeneratorDao;
+    @Resource
+    private SQLServerGeneratorDao sqlServerGeneratorDao;
+    @Resource
+    private PostgreSQLGeneratorDao postgreSQLGeneratorDao;
+
+    @Bean
+    @Primary
+    public GeneratorDao getGeneratorDao(){
+        if("mysql".equalsIgnoreCase(database)){
+            return mySQLGeneratorDao;
+        }else if("oracle".equalsIgnoreCase(database)){
+            return oracleGeneratorDao;
+        }else if("sqlserver".equalsIgnoreCase(database)){
+            return sqlServerGeneratorDao;
+        }else if("postgresql".equalsIgnoreCase(database)){
+            return postgreSQLGeneratorDao;
+        }else {
+            throw new RRException("不支持当前数据库：" + database);
+        }
+    }
+}
